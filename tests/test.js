222193b8af02f3559b01a918388409d3db2d2c41
Evan Dickinson <me@evand.name>
2017-02-25 14:30:15 -0800

    Add a 2nd test case

--- tests/test.js
+++ tests/test.js
@@ -26,8 +26,7 @@ function openSketchFile(filename, done) {
     -- http://www.macscripter.net/viewtopic.php?pid=60364
     considering application responses
       tell application "Sketch"
-        -- TODO: Would be nice to only close temp document, not every doc
-        close every document without saving
+        close "test-cases" without saving
       end tell
     end considering
 
@@ -57,19 +56,26 @@ describe("Basic tests", function() {
   });
   beforeEach(function(done) {
     openSketchFile(tempFilename, done);
-  })
+  });
 
   it("Dummy", function() {});
 
 
-  it("Should work", function(done) {
-    var script = `
+  it("Push one level", function(done) {
+    const PAGE_NAME = "Push one level";
+
+    var args = {
+      PAGE_NAME: PAGE_NAME,
+    };
+    argsCmd = "var args = " + JSON.stringify(args) + ";\n";
+    var script = argsCmd + `
       @import '../Push-Pop.sketchplugin/Contents/Sketch/test-utils.cocoascript';
       @import '../Push-Pop.sketchplugin/Contents/Sketch/script.cocoascript';
 
       var response = {};
 
       var doc = context.document;
+      setCurrentPageByName(doc, args.PAGE_NAME);
       var page = [doc currentPage];
       var group = findLayerNamed([page children], 'Group');
       var oval  = findLayerNamed([page children], 'Oval');
@@ -101,12 +107,12 @@ describe("Basic tests", function() {
     sketch.run(script)
       .then(function(response) {
         var data = response.data;
-        expect(data.beforePush.groupParentName).toBe("Simple push");
-        expect(data.beforePush.ovalParentName).toBe("Simple push");
+        expect(data.beforePush.groupParentName).toBe(PAGE_NAME);
+        expect(data.beforePush.ovalParentName).toBe(PAGE_NAME);
         expect(data.beforePush.groupParentIsOvalParent).toBeTruthy();
 
 
-        expect(data.afterPush.groupParentName).toBe("Simple push");
+        expect(data.afterPush.groupParentName).toBe(PAGE_NAME);
         expect(data.afterPush.ovalParentName).toBe("Group");
         expect(data.afterPush.groupParentIsOvalParent).toBeFalsy();
 
@@ -118,4 +124,67 @@ describe("Basic tests", function() {
         done();
       })
   });
+
+
+ it("Push two levels", function(done) {
+    const PAGE_NAME = "Push two levels";
+
+    var args = {
+      PAGE_NAME: PAGE_NAME,
+    };
+    argsCmd = "var args = " + JSON.stringify(args) + ";\n";
+    var script = argsCmd + `
+      @import '../Push-Pop.sketchplugin/Contents/Sketch/test-utils.cocoascript';
+      @import '../Push-Pop.sketchplugin/Contents/Sketch/script.cocoascript';
+
+      var response = {};
+
+      var doc = context.document;
+      setCurrentPageByName(doc, args.PAGE_NAME);
+      var page = [doc currentPage];
+      var group = findLayerNamed([page children], 'Inner Group');
+      var oval  = findLayerNamed([page children], 'Oval');
+
+      response.beforePush = {
+        // To do JSON stuff, convert from NSString to plain JS String
+        groupParentName: new String(getParent(group).name()),
+        ovalParentName:  new String(getParent(oval).name()),
+
+      };
+
+      // TODO: We'll test Sketch better if we can actually modify the selection programatically.
+      // Catch cases where the selection stops being ordered.
+      var layers = [NSArray arrayWithObjects: group, oval];
+      doPushIn(layers);
+
+
+      response.afterPush = {
+        groupParentName: new String(getParent(group).name()),
+        ovalParentName:  new String(getParent(oval).name()),
+
+      }
+
+      $SD.respond(response)
+    `
+
+    sketch.run(script)
+      .then(function(response) {
+        var data = response.data;
+        expect(data.beforePush.groupParentName).toBe('Outer Group');
+        expect(data.beforePush.ovalParentName).toBe(PAGE_NAME);
+
+
+        expect(data.afterPush.groupParentName).toBe('Outer Group');
+        expect(data.afterPush.ovalParentName).toBe("Inner Group");
+
+        done();
+      })
+      .catch(function(err) {
+        console.log(err);
+        expect(false).toBe(true);
+        done();
+      })
+  });
+
+
 });
